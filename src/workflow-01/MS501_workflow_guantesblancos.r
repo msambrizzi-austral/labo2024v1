f494709c5188dc36b684e13fc8caf0b9efef70f7
msambrizzi-austral <msambrizziorduna@mail.austral.edu.ar>
2024-05-05 14:53:18 +0000

    updates

--- src/workflow-01/MS501_workflow_guantesblancos.r
+++ src/workflow-01/MS501_workflow_guantesblancos.r
@@ -14,7 +14,7 @@ require("ParamHelpers")
 envg <- env()
 
 envg$EXPENV <- list()
-envg$EXPENV$exp_dir <- "~/buckets/b1/exp/"
+envg$EXPENV$exp_dir <- "~/buckets/b2/exp/"
 envg$EXPENV$wf_dir <- "~/buckets/b1/flow/"
 envg$EXPENV$wf_dir_local <- "~/flow/"
 envg$EXPENV$repo_dir <- "~/labo2024v1/"
