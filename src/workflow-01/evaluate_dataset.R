5ec701aed1b1f4b848d1f5042315c88611ae1680
msambrizzi-austral <msambrizzi@gmail.com>
2024-05-10 04:21:49 +0000

    update evaluate

--- src/workflow-01/evaluate_dataset.R
+++ src/workflow-01/evaluate_dataset.R
@@ -3,6 +3,7 @@ library(data.table)
 library(dplyr)
 library(ggplot2)
 library(lubridate)
+library(scales) 
 
 setwd("~/buckets/b1")
 
@@ -36,9 +37,35 @@ baja_por_fecha <- dataset_filtrado %>%
             total_clientes = n()) %>%
   mutate(proporcion_baja2 = baja2_count / (total_clientes + 2))
 
-# Graficar la proporción de 'BAJA+2' por fecha
-ggplot(baja_por_fecha, aes(x = fecha, y = proporcion_baja2)) +
-  geom_line() +  # Línea para conectar los puntos
-  geom_point() + # Puntos en cada dato
-  labs(x = "Fecha", y = "Proporción de BAJA+2", title = "Proporción de BAJA+2 por Fecha") +
-  theme_minimal()  # Estilo minimalista para el gráfico
+
+# Crear el gráfico base
+p <- ggplot(baja_por_fecha, aes(x = fecha, y = proporcion_baja2)) +
+  geom_line(color = 'deepskyblue4', size = 1) +  # Línea para conectar los puntos
+  geom_point(color = 'deepskyblue', size = 2) + # Puntos en cada dato
+  labs(x = "Fecha", y = "Proporción de BAJA+2 por Mes", title = "BAJA+2/QClientes en T+2") +
+  theme_minimal() +  # Estilo minimalista para el gráfico
+  theme(plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+        axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12),
+        axis.text.x = element_text(angle = 90, hjust = 1, size = 9),
+        axis.text.y = element_text(size = 9),
+        legend.position = "bottom") +
+  scale_x_date(labels = date_format("%Y-%m"), breaks = date_breaks("2 months"))  # Cada 2 meses
+
+# Añadir los dos geom_rect con la estética fill mapeada para la leyenda
+p <- p + geom_rect(aes(xmin = as.Date("2020-03-01"), xmax = as.Date("2020-04-30"), ymin = -Inf, ymax = Inf,
+                       fill = "ASPO"), alpha = 0.03)
+
+p <- p + geom_rect(aes(xmin = as.Date("2020-05-01"), xmax = as.Date("2021-02-28"), ymin = -Inf, ymax = Inf,
+                       fill = "DISPO"), alpha = 0.03)
+
+# Configurar la leyenda con scale_fill_manual
+p <- p + scale_fill_manual("Descripción de Periodos",
+                           values = c("ASPO" = 'deepskyblue', 
+                                      "DISPO" = 'deepskyblue4'))
+# Ajustar la leyenda para que refleje la transparencia
+p <- p + guides(fill = guide_legend(override.aes = list(alpha = 0.6)))
+
+# Mostrar el gráfico
+print(p)
+
